7e1aa8b05b8773a28fcc22862db769f6b1a590cd
Aidan Nabass <ai.nabass@gmail.com>
2015-04-28 14:59:15 -0700

    Initial Import

---
+++ AISpritePacker/AISpritePacker/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace AISpritePacker
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MenuItem_Click_Open(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void MenuItem_Click_Import(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Multiselect = true;
+			if (openFileDialog.ShowDialog() == true)
+			{
+				foreach (string filename in openFileDialog.FileNames)
+				{
+					Image cardImage = new Image();
+					cardImage.Source = new BitmapImage(new Uri(System.IO.Path.GetFullPath(filename)));
+					Canvas_Sprites.Children.Add(cardImage);
+					Canvas_Sprites.Children[0].AllowDrop = true;
+
+				}
+			}
+		}
+	}
+}
